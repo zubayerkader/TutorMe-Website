e06b82a951170594b21881df34fd3ff9345cd4f3
Kazi Zubayer Quader <zubayerkader@Kazis-MacBook-Air.local>
2019-07-11 19:20:45 -0700

    Student Sign Up almost done

--- back-end/phpscripts/main.php
+++ back-end/phpscripts/main.php
@@ -4,255 +4,38 @@
     </head>
     <body>
         <?php
-
-        if(isset($_POST['submit'])){
+            $str_json = file_get_contents('php://input');
+            $json = json_decode($str_json);
             
-            $data_missing = array();
-            
-            if(empty($_POST['first_name'])){
+            $fname = $json->fname;
+            $lname = $json->lname;
+            $email = $json->email;
+            $password = $json->pass;
+            $phone = $json->phone;
+            $gender = $json->gender;
+            $department = $json->major;
+            $school = $json->school;
 
-                // Adds name to array
-                $data_missing[] = 'First Name';
-
-            } else {
-
-                // Trim white space from the name and store the name
-                $f_name = trim($_POST['first_name']);
-
-            }
-
-            if(empty($_POST['last_name'])){
-
-                // Adds name to array
-                $data_missing[] = 'Last Name';
-
-            } else{
-
-                // Trim white space from the name and store the name
-                $l_name = trim($_POST['last_name']);
-
-            }
-
-            if(empty($_POST['email'])){
-
-                // Adds name to array
-                $data_missing[] = 'Email';
-
-            } else {
-
-                // Trim white space from the name and store the name
-                $email = trim($_POST['email']);
-
-            }
-
-            if(empty($_POST['street'])){
-
-                // Adds name to array
-                $data_missing[] = 'Street';
-
-            } else {
-
-                // Trim white space from the name and store the name
-                $street = trim($_POST['street']);
-
-            }
-
-            if(empty($_POST['city'])){
-
-                // Adds name to array
-                $data_missing[] = 'City';
-
-            } else {
-
-                // Trim white space from the name and store the name
-                $city = trim($_POST['city']);
-
-            }
-
-            if(empty($_POST['state'])){
-
-                // Adds name to array
-                $data_missing[] = 'State';
-
-            } else {
-
-                // Trim white space from the name and store the name
-                $state = trim($_POST['state']);
-
-            }
-
-            if(empty($_POST['zip'])){
-
-                // Adds name to array
-                $data_missing[] = 'Zip Code';
-
-            } else {
-
-                // Trim white space from the name and store the name
-                $zip = trim($_POST['zip']);
-
-            }
-
-            if(empty($_POST['phone'])){
-
-                // Adds name to array
-                $data_missing[] = 'Phone Number';
-
-            } else {
-
-                // Trim white space from the name and store the name
-                $phone = trim($_POST['phone']);
-
-            }
-
-            if(empty($_POST['birth_date'])){
-
-                // Adds name to array
-                $data_missing[] = 'Birth Date';
-
-            } else {
-
-                // Trim white space from the name and store the name
-                $b_date = trim($_POST['birth_date']);
-
-            }
-
-            if(empty($_POST['sex'])){
-
-                // Adds name to array
-                $data_missing[] = 'Sex';
-
-            } else {
-
-                // Trim white space from the name and store the name
-                $sex = trim($_POST['sex']);
-
-            }
-
-            if(empty($_POST['lunch'])){
-
-                // Adds name to array
-                $data_missing[] = 'Lunch Cost';
-
-            } else {
-
-                // Trim white space from the name and store the name
-                $lunch = trim($_POST['lunch']);
-
-            }
-            
-            if(empty($data_missing)){
-                
-                require_once('../mysqli_connect.php');
-                
-                $query = "INSERT INTO students (first_name, last_name, email,
-                street, city, state, zip, phone, birth_date, sex, date_entered,
-                lunch_cost, student_id) VALUES (?, ?, ?,
-                ?, ?, ?, ?, ?, ?, ?, NOW(), ?, NULL)";
-                
-                $stmt = mysqli_prepare($dbc, $query);
-                
-                //i Integers
-                //d Doubles
-                //b Blobs
-                //s Everything Else
-                
-                mysqli_stmt_bind_param($stmt, "ssssssisssd", $f_name,
-                                       $l_name, $email, $street, $city,
-                                       $state, $zip, $phone, $b_date,
-                                       $sex, $lunch);
-                
-                mysqli_stmt_execute($stmt);
-                
-                $affected_rows = mysqli_stmt_affected_rows($stmt);
-                
-                if($affected_rows == 1){
-                    
-                    echo 'Student Entered';
-                    
-                    mysqli_stmt_close($stmt);
-                    
-                    mysqli_close($dbc);
-                    
-                } else {
-                    
-                    echo 'Error Occurred<br />';
-                    echo mysqli_error();
-                    
-                    mysqli_stmt_close($stmt);
-                    
-                    mysqli_close($dbc);
-                    
+            require_once('conn.php');
+            $query = "INSERT INTO Student (fname, lname, email, phone, gender, department, school) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            $stmt  = $dbc->prepare($query);
+            if ($stmt)
+            {
+                $stmt->bind_param("sssdsss", $fname, $lname, $email, $phone, $gender, $department, $school);
+                if ($stmt->execute())
+                {
+                    echo "<br>No of records inserted : " . $dbc->affected_rows;
+                    echo "<br>Insert ID : " . $dbc->insert_id;
                 }
-                
-            } else {
-                
-                echo 'You need to enter the following data<br />';
-                
-                foreach($data_missing as $missing){
-                    
-                    echo "$missing<br />";
-                    
-                }
-                
+                else
+                    echo $dbc->error;
             }
-            
-        }
-
+            else
+                echo $dbc->error;
+            //i Integers
+            //d Doubles
+            //b Blobs
+            //s Everything Else
         ?>
-
-        <form action="http://localhost/studentadded.php" method="post">
-            
-            <b>Add a New Student</b>
-            
-            <p>First Name:
-        <input type="text" name="first_name" size="30" value="" />
-        </p>
-
-        <p>Last Name:
-        <input type="text" name="last_name" size="30" value="" />
-        </p>
-
-        <p>Email:
-        <input type="text" name="email" size="30" value="" />
-        </p>
-
-        <p>Street:
-        <input type="text" name="street" size="30" value="" />
-        </p>
-
-        <p>City:
-        <input type="text" name="city" size="30" value="" />
-        </p>
-
-        <p>State (2 Characters):
-        <input type="text" name="state" size="30" maxlength="2" value="" />
-        </p>
-
-        <p>Zip Code:
-        <input type="text" name="zip" size="30" maxlength="5" value="" />
-        </p>
-
-        <p>Phone Number:
-        <input type="text" name="phone" size="30" value="" />
-        </p>
-
-        <p>Birth Date (YYYY-MM-DD):
-        <input type="text" name="birth_date" size="30" value="" />
-        </p>
-
-        <p>Sex (M or F):
-        <input type="text" name="sex" size="30" maxlength="1" value="" />
-        </p>
-
-        <p>Lunch Cost:
-        <input type="text" name="lunch" size="30" value="" />
-        </p>
-
-        <p>
-            <input type="submit" name="submit" value="Send" />
-        </p>
-            
-        </form>
     </body>
 </html>
--- front-end/scripts/main.js
+++ front-end/scripts/main.js
@@ -6,34 +6,34 @@ function studentSignUp()
 		load sign in page
 	*/
 
-	var fname = document.getElementbyID("fname").value;
+	var Fname = document.getElementById("fname").value;
 
-	var lname = document.getElementbyID("lname").value;
+	var Lname = document.getElementById("lname").value;
 
-	var email = document.getElementbyID("email").value;
+	var Email = document.getElementById("email").value;
 
-	var pass = document.getElementbyID("pass").value;
+	var Pass = document.getElementById("password").value;
 
-	var cellphone = document.getElementbyID("cellphone").value;
+	var Phone = document.getElementById("phone").value;
 
-	var gender;
-	var radios = document.getElementsByName('genderS');
+	var Gender;
+	var radios = document.getElementsByName("gender");
 	for (var i = 0, length = radios.length; i < length; i++) 
 	{
     	if (radios[i].checked) 
     	{
         	// do whatever you want with the checked radio
-        	gender = radios[i].value;
+        	Gender = radios[i].value;
         	// only one radio can be logically checked, don't check the rest
         	break;
     	}
 	}
 
-	var selectMajor = document.getElementbyID("selectMajor");
-	var major = selectedMajor.options[selectedMajor.selectedIndex].value;
+	var selectedMajor = document.getElementById("selectMajor");
+	var Major = selectedMajor.options[selectedMajor.selectedIndex].value;
 
-	var selectSchool = document.getElementbyID("selectSchool");
-	var school = selectSchool.options[selectSchool.selectedIndex].value;
+	var selectedSchool = document.getElementById("selectSchool");
+	var School = selectedSchool.options[selectedSchool.selectedIndex].value;
 
 	/*var queryString = "insertSignUp.php?fname=" + fname +
 									"&lname=" + lname +
@@ -42,29 +42,31 @@ function studentSignUp()
 									"&cellphone=" + cellphone +
 									"&major=" + major +
 									"&school=" + school;*/
+ 
+	var obj = {
+		fname: Fname,
+		lname: Lname,
+		email: Email,
+		pass: Pass,
+		phone: Phone,
+		gender: Gender,
+		major: Major,
+		school: School
+	};
 
-	var xmlhttp = new XMLHttpRequest();
-	var url = "url";
-	xmlhttp.open("POST", url, true);
+	var data = JSON.stringify(obj);
+
+	document.getElementById("demo1").innerHTML = data;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", "http://localhost:8888/main.php", true);
 	xhr.setRequestHeader("Content-Type", "application/json");
 	xhr.onreadystatechange = function () 
 	{
-	    if (xhr.readyState === 4 && xhr.status === 200) 
-	    {
-	        var json = JSON.parse(xhr.responseText);
-	        console.log(json.email + ", " + json.password);
-	    }
+	    if (this.readyState == 4 && this.status == 200) 
+			document.getElementById("demo2").innerHTML = this.responseText;
 	};
-	var obj = {
-		"fname": ""
-	};
-	var data = JSON.stringify(obj);
 	xhr.send(data);
-
-	
-	
-	
-	
 }
 
 function studentSignIn(){
